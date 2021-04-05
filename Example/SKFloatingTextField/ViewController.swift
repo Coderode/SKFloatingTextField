2084eb4f510e0082e436383c1adb5609638bda1e
coderode <sk9958814616@gmail.com>
2021-04-05 19:20:01 +0530

    Initial commit

---
+++ Example/SKFloatingTextField/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SKFloatingTextField
+//
+//  Created by coderode on 04/05/2021.
+//  Copyright (c) 2021 coderode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
